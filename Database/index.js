de08f7f4eba0d13a16ae666454e6244de410baa3
https://github.com/EnesDaca <enesdaca@hotmail.com>
2023-10-13 21:08:30 +0200

    mysql webbaseupdate v4

--- Database/index.js
+++ Database/index.js
@@ -2,7 +2,7 @@ import mysql from "mysql";
 
 export const connection = mysql.createPool({
   host: "https://databases-auth.000webhost.com/index.php?route=/database/structure&db=id21395682_dacamysq",
-  user: "clothing-store-server",
+  user: "id21395682_daca",
   password: "Tehnicar33=",
   database: "id21395682_dacamysq",
   // port: 3307,
