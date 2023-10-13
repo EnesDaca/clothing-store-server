5bf3e2a8f583e29844d686b9fe5498e98cc5555b
https://github.com/EnesDaca <enesdaca@hotmail.com>
2023-10-13 21:04:10 +0200

    mysql webbaseupdate v3

--- Database/index.js
+++ Database/index.js
@@ -1,7 +1,7 @@
 import mysql from "mysql";
 
 export const connection = mysql.createPool({
-  host: "files.000webhost.com",
+  host: "https://databases-auth.000webhost.com/index.php?route=/database/structure&db=id21395682_dacamysq",
   user: "clothing-store-server",
   password: "Tehnicar33=",
   database: "id21395682_dacamysq",
