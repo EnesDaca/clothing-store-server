9f7f6d3c7a2e9be4fa4a74955a4b0502e54301e2
https://github.com/EnesDaca <enesdaca@hotmail.com>
2023-10-17 20:33:23 +0200

    products db update

--- Database/index.js
+++ Database/index.js
@@ -1,10 +1,20 @@
+// require("dotenv").config();
 import mysql from "mysql";
 
+// export const connection = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DBNAME,
+//   port: 3306,
+//   multipleStatements: true,
+// });
+
 export const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DBNAME,
+  host: "localhost",
+  user: "root",
+  password: "tehnicar33",
+  database: "clothingstore",
   port: 3306,
   multipleStatements: true,
 });
